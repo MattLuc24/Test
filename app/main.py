1e96163c48ce9ea29c8b18c0dc5b6bf4770aa1ef
MattLuc24 <Matthew.F.Lucien@gmail.com>
2024-11-20 22:08:06 -0500

    Update to wait for client connection and send "+OK" response

--- app/main.py
+++ app/main.py
@@ -5,10 +5,10 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    # Wait for client connection and send "+OK" response
+    connection, _ = server_socket.accept()
+    connection.sendall(b"+OK\r\n")
 
 
 if __name__ == "__main__":
