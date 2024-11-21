193629d79f64b8abf0c48d1fabacff30c002827a
MattLuc24 <Matthew.F.Lucien@gmail.com>
2024-11-20 22:20:20 -0500

    Update to PONG

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # Wait for client connection and send "+OK" response
     connection, _ = server_socket.accept()
-    connection.sendall(b"+OK\r\n")
+    connection.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
